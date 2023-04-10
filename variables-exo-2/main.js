c949c6a7e27a5123afbdb630f80e9e1b2bba57cc
Yannick Moris <yanndevpro@gmail.com>
2023-04-10 21:07:59 +0200

    exo 2 terminer

---
+++ variables-exo-2/main.js
@@ -0,0 +1,13 @@
+let nom = "toto";
+let age = 30;
+let homme = false;
+
+console.log(`Nom : ${nom}`);
+console.log(`Age : ${age}`);
+
+if (homme === true) {
+    console.log(`Sexe : homme`);
+}else {
+    console.log(`Sexe : femme`);
+}
+
